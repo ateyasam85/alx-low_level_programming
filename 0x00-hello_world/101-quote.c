77a1fa843983dd54dd85acd5dcb755413b119437
ateyasam85 <samsonmario85@gmail.com>
2023-08-10 16:39:14 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 #include<unistd.h>
 
@@ -9,6 +8,6 @@
  */
 int main (void)
 {
-	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59):
-		return (1);
+fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
 }
