111e84a4f06f81b21978d4444341421d1a31ba14
ateyasam85 <samsonmario85@gmail.com>
2023-08-11 16:52:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry level
+* Description: 'this is the program'
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+return (0);
+}
